aab4f0a9e3a790f604d0d1cfcc5abd3da8a005ce
pratik <raut70856@gmail.com>
2025-08-12 17:54:54 +0530

    Full Backend Setup

---
+++ db/db.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const connectDB = require('./index');
+require('dotenv').config({path: './.env'});
+
+connectDB();
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique:true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    
+
+
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 2,
+        maxLength: 30
+    },
+       lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 2,
+        maxLength: 30
+
+    }
+
+
+})
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+ const User = mongoose.model('User', userSchema);
+module.exports = {
+    User,
+    Account
+}
